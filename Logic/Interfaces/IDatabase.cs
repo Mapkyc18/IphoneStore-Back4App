ea07fd6dc984c9f7fb07dc9083a2f7cdc0d518bd
Marcus Galdino <marcusgaldino3@gmail.com>
2024-12-09 12:06:46 -0700

    IDatabase class code Implemented.

---
+++ Logic/Interfaces/IDatabase.cs
@@ -0,0 +1,11 @@
+using testing_final.Logic.Models;
+
+namespace testing_final.Logic.Interfaces;
+
+public interface IDatabase
+{
+    void SaveOrder(Order order);
+    List<Order> GetPendingOrders();
+    void FulfillOrder(int orderId);
+    List<Order> GetOrdersByDate(DateTime date);
+}
