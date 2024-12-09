ab50d6f79a313e2681811ec800bc88de08cb5540
Marcus Galdino <marcusgaldino3@gmail.com>
2024-12-09 12:05:58 -0700

    Item class code Implemented.

---
+++ Logic/Models/Item.cs
@@ -0,0 +1,10 @@
+namespace testing_final.Logic.Models;
+
+public class Item
+{
+    public int ItemId { get; set; }
+    public int OrderId { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+}
